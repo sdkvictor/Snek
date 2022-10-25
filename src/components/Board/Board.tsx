759f78246ce6be355c6c24ac307fce0ea53b56c4
Victor Villarreal <victor.villarreal@wizeline.com>
2022-10-25 13:36:34 -0500

    eslint fixes

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BoardStyled, CellStyled, RowStyled } from './Board.styled';
 
 export interface BoardProps {
@@ -13,10 +13,10 @@ function Board({ grid, character, food }: BoardProps) {
       {grid.map((row, rowIndex) => (
         <RowStyled key={rowIndex}>
           {row.map((cell, cellIndex)=> (
-            (rowIndex == character[0] && cellIndex == character[1] ?
+            (rowIndex === character[0] && cellIndex === character[1] ?
               <CellStyled value={1} key={cellIndex}/> 
               :
-              (rowIndex == food[0] && cellIndex == food[1] ? 
+              (rowIndex === food[0] && cellIndex === food[1] ? 
                 <CellStyled value={2} key={cellIndex}/>
                 : <CellStyled value={cell} key={cellIndex}/>)
               )
